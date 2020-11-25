a341834eb8055947b622ff5ded3fc3dfd2e09a71
Ryanhindman6654 <45013407+Ryanhindman6654@users.noreply.github.com>
2020-11-25 13:14:00 -0800

    Add files via upload

---
+++ application-questions.js
@@ -0,0 +1,8 @@
+module.exports =  [
+"Discord name and tag?",
+"Are you apply for bot admin or support?",
+"Why should we pick you for the position?",
+"What will you bring to the team?",
+"Do you currently use Deathkillers Bot?",
+"How active will you be as a Bot Admin/Support?"
+];
